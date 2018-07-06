3aedf205298cee14f35e6275155dbde3953b3dd4
shuvaev <derdin1997@yandex.ru>
2018-07-06 15:25:10 +0700

    v0.0.2

---
+++ src/main/java/ftc/shift/sample/models/UserLogin.java
@@ -0,0 +1,11 @@
+package ftc.shift.sample.models;
+
+import lombok.*;
+
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class UserLogin {
+    public String userName;
+    public String password;
+}
