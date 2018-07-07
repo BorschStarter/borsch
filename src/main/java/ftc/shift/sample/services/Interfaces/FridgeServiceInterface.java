4ae281acc62136dcb2cf944eb12cb3fd49615b70
Stanislav Abramov <abramov.center@yandex.ru>
2018-07-07 17:56:15 +0700

    v0.0.3

--- src/main/java/ftc/shift/sample/services/Interfaces/FridgeServiceInterface.java
+++ src/main/java/ftc/shift/sample/services/Interfaces/FridgeServiceInterface.java
@@ -10,9 +10,13 @@ import java.util.List;
 
 public interface FridgeServiceInterface {
 
+    //IllegalArgumentException("Вы ввели null");
     Fridge provideUserFridge(String id);
 
+    //IllegalArgumentException("Вы ввели null");
     Fridge addProductInFridge(String id, Product product);
 
+    //IllegalArgumentException("Вы ввели null");
+    //IllegalArgumentException("Продуктов с таким именем нет в базе");
     List<Food> getFoodSearchList(String foodName);
 }
--- src/main/java/ftc/shift/sample/services/Interfaces/UserServiceInterface.java
+++ src/main/java/ftc/shift/sample/services/Interfaces/UserServiceInterface.java
@@ -7,13 +7,22 @@ import ftc.shift.sample.models.UserValidInfo;
 
 public interface UserServiceInterface {
 
+    //IllegalArgumentException("Вы передали null")
+    //IllegalArgumentException("Вы ввели неправильный пароль")
+    //IllegalArgumentException("Пользователя с таким логином не существует")
     Token createToken(UserLogin userLogin);
 
+    //IllegalArgumentException("Вы передали null")
     Boolean checkAccess(UserValidInfo userValidInfo);
 
+    //IllegalArgumentException("Вы передали null")
     UserInfo provideUserInfo(String id);
 
+    //IllegalArgumentException("Вы передали null")
+    //IllegalArgumentException("Пользователя с таким логином не существует")
     UserInfo updateUserInfo(String id, UserInfo userInfo);
 
+    //IllegalArgumentException("Вы передали null")
+    //IllegalArgumentException("Этот логин уже существует")
     void registration(UserLogin userLogin);
 }
