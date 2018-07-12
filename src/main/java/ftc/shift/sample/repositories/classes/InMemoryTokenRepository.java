db7ed28e8df63ca073e30a478b3e2046a7893576
Александр Гусев <alex1232131@mail.ru>
2018-07-12 14:27:06 +0700

    gavnokod

--- src/main/java/ftc/shift/sample/repositories/classes/InMemoryTokenRepository.java
+++ src/main/java/ftc/shift/sample/repositories/classes/InMemoryTokenRepository.java
@@ -1,8 +1,11 @@
 package ftc.shift.sample.repositories.classes;
 
+import ftc.shift.sample.entity.TokenEntity;
 import ftc.shift.sample.models.UserValidInfo;
 import ftc.shift.sample.repositories.interfaces.TokenRepository;
+import ftc.shift.sample.repositories.interfaces.TokenRepositoryEntity;
 import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.*;
@@ -10,29 +13,54 @@ import java.util.*;
 @Repository
 public class InMemoryTokenRepository implements TokenRepository {
 
-    private TreeMap<String, String> tokenCache = new TreeMap<>();
+//    private TreeMap<String, String> tokenCache = new TreeMap<>();
+    @Autowired
+    private TokenRepositoryEntity service;
+
+    public InMemoryTokenRepository() {}
 
     @Override
-    public void addToken(@NonNull final UserValidInfo userValidInfo) {
-        tokenCache.put(userValidInfo.getId(),userValidInfo.getToken());
+    public void addToken(@NonNull final UserValidInfo userValidInfo, Integer userId, String login) {
+
+//        tokenCache.put(userValidInfo.getId(),userValidInfo.getToken());
+        TokenEntity token = new TokenEntity();
+        token.setLogin(login);
+        token.setUserId(userId);
+        token.setToken(userValidInfo.getToken());
+
+        service.save(token);
     }
 
     @Override
     public ArrayList<String> getAllTokensUser(@NonNull final String idUser) {
 
-        ArrayList<String> tokens = new ArrayList<>();
+//        ArrayList<String> tokens = new ArrayList<>();
 
-        for (Map.Entry element: tokenCache.entrySet()) {
-            if (element.getKey().equals(idUser)){
-                tokens.add((String) element.getValue());
+//        for (Map.Entry element: tokenCache.entrySet()) {
+//            if (element.getKey().equals(idUser)){
+//                tokens.add((String) element.getValue());
+//            }
+//        }
+//        return tokens;
+        Iterable<TokenEntity> allTokens = service.findAll();
+        ArrayList<String> result = new ArrayList<>();
+        allTokens.forEach(item ->{
+            if(item.getLogin().equals(idUser)){
+                result.add(item.getToken());
             }
-        }
-        return tokens;
+        });
+        return result;
     }
 
     @Override
     public void deleteToken(@NonNull final UserValidInfo userValidInfo) {
 
-        getAllTokensUser(userValidInfo.getId()).remove(userValidInfo.getToken());
+        Iterable<TokenEntity> allTokens = service.findAll();
+        allTokens.forEach(item ->{
+            if(item.getToken().equals(userValidInfo.getToken())){
+                service.delete(item);
+            }
+        });
+//        getAllTokensUser(userValidInfo.getId()).remove(userValidInfo.getToken());
     }
 }
---
+++ src/main/java/ftc/shift/sample/repositories/interfaces/TokenRepositoryEntity.java
@@ -0,0 +1,7 @@
+package ftc.shift.sample.repositories.interfaces;
+
+import ftc.shift.sample.entity.TokenEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TokenRepositoryEntity extends CrudRepository<TokenEntity, Integer> {
+}
